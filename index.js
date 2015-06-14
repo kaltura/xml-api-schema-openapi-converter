b79805c1124b57a632e2f84888f0de44ff00f4d6
EC2 Default User <ec2-user@ip-172-31-14-135.us-west-2.compute.internal>
2015-06-14 18:43:31 +0000

    Add class conversion

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ Converter.convert({
   to: 'swagger_2',
   source: 'http://www.kaltura.com/api_v3/api_schema.php'
 }, function(err, spec) {
-  console.log(err, spec);
-})
+  if (err) console.log(err);
+  //else console.log(spec.spec);
+});
