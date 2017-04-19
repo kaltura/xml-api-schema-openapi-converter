c31f97a54cdec561c91c2e18cb1de8f75be7f2b8
Travis CI <bobby.brennan@gmail.com>
2017-04-19 14:24:47 +0000

    add required annotations

--- refresh.js
+++ refresh.js
@@ -4,9 +4,14 @@ var Kaltura = require('./lib/kaltura-spec');
 Converter.Formats['kaltura'] = Kaltura;
 var args = require('yargs').argv;
 
-var SCHEMA_URL = "http://www.kaltura.com/api_v3/api_schema.php";
+var SCHEMA_URL = process.env.TARGET_API === 'ott'
+  ? "https://tvpapi-us-preprod.ott.kaltura.com/v4_1/clientlibs/KalturaClient.xml"
+  : "http://www.kaltura.com/api_v3/api_schema.php";
+var DEFAULT_OUTPUT = process.env.TARGET_API === 'ott'
+  ? "out/ott.openapi.json"
+  : "out/ovp.openapi.json";
 args.schema = args.schema || SCHEMA_URL;
-args.output = args.output || __dirname + '/out/swagger.json';
+args.output = args.output || DEFAULT_OUTPUT;
 
 Converter.convert({
   from: 'kaltura',
