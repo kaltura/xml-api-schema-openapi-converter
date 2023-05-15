e160493840e0c9689e7b052693664662e2a9f754
Zohar Babin <zohar.babin@kaltura.com>
2023-05-15 23:07:56 +0300

    updated to latest node version (20.1)

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-module.exports = {
-  swagger: require('./out/swagger.json'),
-  kalturaFormat: require('./lib/kaltura-spec.js'),
+// Import necessary modules
+import swagger from './out/swagger.json';
+import kalturaFormat from './lib/kaltura-spec.js';
+
+/**
+ * This module exports swagger and kalturaFormat configurations.
+ *
+ * @swagger: The swagger configuration file, parsed from JSON.
+ * @kalturaFormat: The Kaltura specification module.
+ */
+export {
+  swagger,
+  kalturaFormat
 };
