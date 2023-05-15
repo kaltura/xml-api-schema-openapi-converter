5a5f5e9c1c95f053da289dba4a07fa4a54855a28
Zohar Babin <zohar.babin@kaltura.com>
2023-05-15 22:31:02 +0300

    upgraded to es6 compatible code and improved inline docs and readme

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-# xml-api-schema-openapi-converter
-Converts the Kaltura API schema XML to an Open API document
+# XML API Schema to OpenAPI 3 Converter
 
-## Usage
+ A tool that convert the Kaltura API XML-based schema to an OpenAPI 3 compliant json file.
+
+## Requirements
+
+node v14.21.3 (npm v6.14.18)  
+**Note: This project is designed to run as a local dev tool**  
+It should never be run in production servers, only as a local dev tool.
+
+## To install
+
+```bash
+npm install
 ```
+
+## To run
+
+```bash
+node refresh.js --target ovp --schema <xml-schema-file> --output <output-file>
+```
+
+### For example (Kaltura.com core services schema)
+
+```bash
 node refresh.js --target ovp --schema https://www.kaltura.com/api_v3/api_schema.php --output ./openapi.json
 ```
 
-* `--target` shoudl be `ovp` or `ott` (default `ovp`)
-* `--schema` argument can be a URL or a local file (default is the schema hosted on kaltura.com)
-* `--output` is the destination file (default is in the `./out/` folder)
+### Parameters
 
+* `--target` Should be `ovp` or `ott` (default `ovp`)
+* `--schema` Argument can be a URL or a local file (default is the schema hosted on kaltura.com)
+* `--output` Is the destination file (default is in the `./out/` folder)
+* `--novalidate` Will skip validation at the end of generating the new schema
--- refresh.js
+++ refresh.js
@@ -1,30 +1,62 @@
-var args = require('yargs').argv;
+// Import necessary libraries
+const yargs = require('yargs');
+const fs = require('fs');
+const Converter = require('api-spec-converter');
+const Kaltura = require('./lib/kaltura-spec');
+
+/**
+ * Get command-line arguments
+ * @type {Object}
+ */
+const args = yargs.argv;
+
+/**
+ * Set TARGET_API environment variable from command-line arguments,
+ * existing environment variable, or default to 'ovp'
+ * @type {string}
+ */
 process.env.TARGET_API = args.target || process.env.TARGET_API || 'ovp';
 
-var FS = require('fs');
-var Converter = require('api-spec-converter');
-var Kaltura = require('./lib/kaltura-spec');
+// Add Kaltura format to converter
 Converter.Formats['kaltura'] = Kaltura;
 
-var SCHEMA_URL = process.env.TARGET_API === 'ott'
+/**
+ * Set schema URL based on TARGET_API environment variable
+ * @type {string}
+ */
+const schemaUrl = process.env.TARGET_API === 'ott'
   ? "https://tvpapi-us-preprod.ott.kaltura.com/v4_1/clientlibs/KalturaClient.xml"
   : "http://www.kaltura.com/api_v3/api_schema.php";
-var DEFAULT_OUTPUT = process.env.TARGET_API === 'ott'
-  ? "out/ott.openapi.json"
-  : "out/ovp.openapi.json";
-args.schema = args.schema || SCHEMA_URL;
-args.output = args.output || DEFAULT_OUTPUT;
 
+/**
+ * Set default output file based on TARGET_API environment variable
+ * @type {string}
+ */
+const defaultOutput = process.env.TARGET_API === 'ott'
+  ? "./out/ott.openapi.json"
+  : "./out/ovp.openapi.json";
+
+// Use provided schema and output file, or defaults if not provided
+args.schema = args.schema || schemaUrl;
+args.output = args.output || defaultOutput;
+
+/**
+ * Convert Kaltura API spec to Swagger 2.0 spec
+ */
 Converter.convert({
   from: 'kaltura',
   to: 'swagger_2',
   source: args.schema,
-}, function(err, spec) {
+}, (err, spec) => {
   if (err) throw err;
-  FS.writeFileSync(args.output, spec.stringify({order: 'alpha'}));
+
+  // Write converted spec to output file
+  fs.writeFileSync(args.output, spec.stringify({order: 'alpha'}));
+
+  // Validate the converted spec. if --novalidate option is set, skip this verification
   if (!args.novalidate) {
     console.log('Done, validating...');
-    spec.validate(function(errs, warnings) {
+    spec.validate((errs, warnings) => {
       if (errs) console.log("ERRORS", JSON.stringify(errs, null, 2));
     })
   }
